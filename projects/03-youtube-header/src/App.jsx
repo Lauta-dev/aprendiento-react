4f450343a12ad6bc0bb68f03ab96d0e1f05d9529
lautaro <you@example.com>
2023-06-19 16:59:25 -0300

    a

---
+++ projects/03-youtube-header/src/App.jsx
@@ -0,0 +1,15 @@
+import { YoutubeSvg } from "../public/youtube"
+import { SearchBar } from "./SearchBar"
+
+export function App() {
+  return (
+    <div>
+      <div>
+        <YoutubeSvg />
+      </div>
+      <div>
+        <SearchBar />
+      </div>
+    </div>
+  )
+}
