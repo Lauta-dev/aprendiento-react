ec4a45c206e4ffe67a895abf260c30b4121b55a0
lautaro <you@example.com>
2023-07-17 17:35:51 -0300

    Primer commit

--- projects/prueba-tecnica-react/src/service/fact.js
+++
@@ -1,7 +0,0 @@
-const FACT_API_URL = 'https://catfact.ninja/fact'
-
-export const factApi = async () => {
-  const fetchAwait = await fetch(FACT_API_URL)
-  const data = await fetchAwait.json()
-  return data.fact
-}
